cfccf8bbfebb5e2c3c887141bd18033d70ac0e3d
ogetijyothika <ogetijyothika2002@gmail.com>
2024-05-08 13:42:50 +0530

    created cookie-cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning Project to predict Trip Duration of Taxis in NYC',
+    author='Jyothika',
+    license='MIT',
+)
